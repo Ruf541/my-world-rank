43e4c1aaee23e88edaf7dba8df5b2941a6016924
Ruf541 <rufusdrew3@gmail.com>
2021-05-12 22:24:42 +0530

    Fix Link Issue

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -5,7 +5,7 @@ import {
 } from "@material-ui/icons";
 import Styles from "./CountriesTable.module.css";
 import { useState } from "react";
-import Link from "next/Link";
+import Link from "next/link";
 
 const orderBy = (countries, value, direction) => {
   if (direction === "asc") {
